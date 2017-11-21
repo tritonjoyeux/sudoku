5f9cf220ce39db9777f1b7797bb70ee435c80c8b
Jeandaux Paul <paul.jeandaux@gmail.com>
2017-11-21 14:34:09 +0000

    add goroutines

--- sudoku.go
+++ sudoku.go
@@ -2,60 +2,73 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"time"
-	"os"
 	"io/ioutil"
+	"os"
+	"runtime"
+	"strconv"
+	"sync"
+	"time"
 )
 
 type Sudoku struct {
-	Grid [9][9]int
-	Name string
+	Grid    [9][9]int
+	Name    string
+	Elapsed time.Duration
 }
 
-func (this *Sudoku) Display() {
-	line := ""
-	for indexY, valueY := range this.Grid {
-		if indexY == 0 {
-			line += "╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗"
+func displaySudoku(ch2 chan Sudoku, wg *sync.WaitGroup) {
+	for {
+		var base Sudoku
+		base = <-ch2
+
+		fmt.Println(base.Name)
+
+		line := ""
+		for indexY, valueY := range base.Grid {
+			if indexY == 0 {
+				line += "╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗"
+				fmt.Println(line)
+				line = ""
+			}
+
+			if indexY == 3 || indexY == 6 {
+				line += "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣"
+				fmt.Println(line)
+				line = ""
+			}
+			for indexX, valueX := range valueY {
+				if indexX == 0 {
+					line += "║"
+				}
+				if indexX == 3 || indexX == 6 {
+					line += "║"
+				}
+
+				if valueX != 0 {
+					line += " " + strconv.Itoa(valueX) + " "
+				} else {
+					line += " ░ "
+				}
+
+				if indexX != 2 && indexX != 5 && indexX != 8 {
+					line += "│"
+				}
+			}
+			line += "║"
 			fmt.Println(line)
 			line = ""
-		}
-
-		if indexY == 3 || indexY == 6 {
-			line += "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣"
-			fmt.Println(line)
-			line = ""
-		}
-		for indexX, valueX := range valueY {
-			if indexX == 0 {
-				line += "║"
-			}
-			if indexX == 3 || indexX == 6 {
-				line += "║"
-			}
-
-			if valueX != 0 {
-				line += " " + strconv.Itoa(valueX) + " "
-			} else {
-				line += " ░ "
-			}
-
-			if indexX != 2 && indexX != 5 && indexX != 8 {
-				line += "│"
+			if indexY != 2 && indexY != 5 && indexY != 8 {
+				line += "╠───┼───┼───╬───┼───┼───╬───┼───┼───╣"
+				fmt.Println(line)
+				line = ""
 			}
 		}
-		line += "║"
+		line += "╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝"
 		fmt.Println(line)
-		line = ""
-		if indexY != 2 && indexY != 5 && indexY != 8 {
-			line += "╠───┼───┼───╬───┼───┼───╬───┼───┼───╣"
-			fmt.Println(line)
-			line = ""
-		}
+		fmt.Println("The solver function took\033[31m", base.Elapsed)
+		fmt.Println("\033[0m")
+		wg.Done()
 	}
-	line += "╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝"
-	fmt.Println(line)
 }
 
 func (this *Sudoku) Check() bool {
@@ -81,19 +94,19 @@ func (this *Sudoku) Check() bool {
 		}
 	}
 
-	for cadX := 0 ; cadX < 3 ; cadX++ {
-	    for cadY := 0 ; cadY < 3 ; cadY++ {
-	        acc := make(map[int]bool)
-	        for x := cadX*3; x < 3; x++ {
-	            for y := cadY*3; y < 3; y++ {
-    	            val := this.Grid[x][y]
-        			if acc[val] && val != 0 {
-        				return false
-        			}
-        			acc[val] = true
-    	        }
-	        }
-	    }
+	for cadX := 0; cadX < 3; cadX++ {
+		for cadY := 0; cadY < 3; cadY++ {
+			acc := make(map[int]bool)
+			for x := cadX * 3; x < 3; x++ {
+				for y := cadY * 3; y < 3; y++ {
+					val := this.Grid[x][y]
+					if acc[val] && val != 0 {
+						return false
+					}
+					acc[val] = true
+				}
+			}
+		}
 	}
 	return true
 }
@@ -135,7 +148,7 @@ func (this *Sudoku) solveRecursion(coords [][2]int) bool {
 
 func (this *Sudoku) checkCoord(cy int, cx int, nVal int) bool {
 	// Line
-	for x := 0 ; x < 9 ; x++ {
+	for x := 0; x < 9; x++ {
 		val := this.Grid[cy][x]
 		if val == nVal {
 			return false
@@ -143,7 +156,7 @@ func (this *Sudoku) checkCoord(cy int, cx int, nVal int) bool {
 	}
 
 	// Col
-	for y := 0 ; y < 9 ; y++ {
+	for y := 0; y < 9; y++ {
 		val := this.Grid[y][cx]
 		if val == nVal {
 			return false
@@ -155,7 +168,7 @@ func (this *Sudoku) checkCoord(cy int, cx int, nVal int) bool {
 	bx := cx - (cx % 3)
 	for y := 0; y < 3; y++ {
 		for x := 0; x < 3; x++ {
-			val := this.Grid[by + y][bx + x]
+			val := this.Grid[by+y][bx+x]
 			if val == nVal {
 				return false
 			}
@@ -167,13 +180,13 @@ func (this *Sudoku) checkCoord(cy int, cx int, nVal int) bool {
 func getFolderSudoku() []Sudoku {
 	var listSudoku []Sudoku
 	files, err := ioutil.ReadDir(path)
-    if err != nil {
+	if err != nil {
 		fmt.Println(err)
-        os.Exit(0)
-    }
+		os.Exit(0)
+	}
 	sudoku := Sudoku{}
-    for _, f := range files {
-        content, err := ioutil.ReadFile(path + f.Name())
+	for _, f := range files {
+		content, err := ioutil.ReadFile(path + f.Name())
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(0)
@@ -184,18 +197,18 @@ func getFolderSudoku() []Sudoku {
 		x := 0
 		y := 0
 
-		for _, ch := range content{
+		for _, ch := range content {
 			str := string(ch)
-			if(str == "."){
+			if str == "." {
 				sudoku.Grid[y][x] = 0
-			}else if str != "\n" {
+			} else if str != "\n" {
 				sudoku.Grid[y][x], _ = strconv.Atoi(str)
 			}
-			if(x > 8){
+			if x > 8 {
 				x = 0
-				y = y+1
-			}else {
-				x = x+1
+				y = y + 1
+			} else {
+				x = x + 1
 			}
 		}
 		listSudoku = append(listSudoku, sudoku)
@@ -206,18 +219,36 @@ func getFolderSudoku() []Sudoku {
 const path = "./example/"
 
 func main() {
+	var wg sync.WaitGroup
+
 	listSudoku := getFolderSudoku()
-	for _, base := range listSudoku {
-		go sudokuIChooseYou(base)
+
+	var ch chan Sudoku
+	ch = make(chan Sudoku)
+
+	var ch2 chan Sudoku
+	ch2 = make(chan Sudoku)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go threadSudoku(ch, &wg, ch2)
 	}
+
+	go displaySudoku(ch2, &wg)
+
+	for _, base := range listSudoku {
+		wg.Add(1)
+		ch <- base
+	}
+	wg.Wait()
 }
 
-func sudokuIChooseYou(base Sudoku) {
-	start := time.Now()
-	base.Solve()
-	elapsed := time.Since(start) // Time spend by solveSudoku
-	fmt.Println(base.Name)
-	base.Display()
-	fmt.Println("The solver function took\033[31m", elapsed)
-	fmt.Println("\033[0m")
+func threadSudoku(ch chan Sudoku, wg *sync.WaitGroup, ch2 chan Sudoku) {
+	for {
+		var base Sudoku
+		base = <-ch
+		start := time.Now()
+		base.Solve()
+		base.Elapsed = time.Since(start) // Time spend by solveSudoku
+		ch2 <- base
+	}
 }
